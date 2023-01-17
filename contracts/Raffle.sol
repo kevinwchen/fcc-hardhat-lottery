7ac09bc02852ffd84a0bb9e8f0ac9caf2d30af6a
Kevin <47770053+kevinwchen@users.noreply.github.com>
2023-01-17 22:32:19 +1100

    Complete raffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -15,6 +15,13 @@ import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
 error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
+
+/** @title A sample Raffle Contract
+ * @author Kevin Chen
+ * @notice This contract is for creating an untamperable decentralised lottery
+ * @dev This implements Chainlink VRF V2 and Automation
+ */
 
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     /* Type Declaraitons */
@@ -45,6 +52,8 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     event RequestedRaffleWinner(uint256 indexed requestId);
     event WinnerPicked(address indexed winner);
 
+    /* Functions */
+
     /* Constructor */
     constructor(
         address vrfCoordinatorV2,
@@ -64,7 +73,6 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         i_interval = interval;
     }
 
-    /* Functions */
     function enterRaffle() public payable {
         // Check entrance fee requirement
         if (msg.value < i_entranceFee) {
@@ -90,16 +98,27 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
      * 4. The lottery should be in an "open" state
      */
     function checkUpkeep(
-        bytes calldata /* checkData */
-    ) external override returns (bool upKeepNeeded, bytes memory /* performData */) {
+        bytes memory /* checkData */
+    ) public override returns (bool upkeepNeeded, bytes memory /* performData */) {
         bool isOpen = (s_raffleState == RaffleState.OPEN);
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
         bool hasPlayers = (s_players.length > 0);
         bool hasBalance = address(this).balance > 0;
-        bool upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
-    function requestRandomWinner() external {
+    /**
+     * @dev This function requests for upkeep when upkeepNeeded is true
+     */
+    function performUpkeep(bytes calldata /* performData */) external override {
+        (bool upkeepNeeded, ) = checkUpkeep(""); // Validate upkeep is needed internally
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
         // Request random number
         // Do something with random number
         // 2 transaction process
@@ -124,6 +143,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
@@ -142,5 +162,25 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getNumPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
